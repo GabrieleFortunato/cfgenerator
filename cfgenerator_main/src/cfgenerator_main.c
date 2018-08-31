59a61c1b760440e5e2af26ac35330d69cb584322
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-08-31 19:06:38 +0200

    Commit del 31 agosto 2018 re 19,06

--- cfgenerator_main/src/cfgenerator_main.c
+++ cfgenerator_main/src/cfgenerator_main.c
@@ -29,13 +29,9 @@ void access(){
 	fclose(file);
 }
 
-void not_found(){
-	file_not_found();
-}
-
 void print_cfgenerator(FILE* file){
 	intro();
-	(file==NULL)?not_found():
+	(file==NULL)?file_not_found():
 			print_result(get_sex(),read_birth_place_code(file,get_birth_town()),get_birth_date(),get_surname(),get_name());
 }
 
