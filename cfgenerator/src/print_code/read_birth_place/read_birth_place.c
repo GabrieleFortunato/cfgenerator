b612f6fd73ed87bdfacf2a8b4f8a9be9f539a203
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-09-04 13:50:38 +0200

    Commit del 4 settembre 2018 ore 13,50

--- cfgenerator/src/print_code/read_birth_place/read_birth_place.c
+++ cfgenerator/src/print_code/read_birth_place/read_birth_place.c
@@ -10,23 +10,23 @@
 static const int FIFTEEN = 50;
 static const char* PAUSE = "pause";
 static const char* FILE_NOT_FOUND = "File dei codici catastali non trovato\n";
-static const int fourtysix = 46;
-static const int zero = 0;
-static const int one = 1;
-static const int five = 5;
+static const int FOURTYSIX = 46;
+static const int FIVE = 5;
+static const int ONE = 1;
+static const int ZERO = 0;
 static const char eos = '\0';
-static const char* virgo = ",";
+static const char* VIRGO = ",";
 
 static void result_code(char* code, char* result){
-	code = strtok(NULL, virgo);
-	code[strlen(code) - one] = eos;
+	code = strtok(NULL, VIRGO);
+	code[strlen(code) - ONE] = eos;
 	strcpy(result, code);
 }
 
 static char* get_result(char* birth_place,char *part,char *result){
-	char *town = strtok(part, virgo);
-	char *code = (char*) malloc((five) * sizeof(char*));
-	if (strcmp(birth_place, town) == zero)
+	char *town = strtok(part, VIRGO);
+	char *code = (char*) malloc((FIVE) * sizeof(char*));
+	if (strcmp(birth_place, town) == ZERO)
 		result_code(code,result);
 	return result;
 }
@@ -36,9 +36,9 @@ char* read_birth_place_code(FILE* places_file,char* birth_place){
 	assert(birth_place!= NULL);
 	assert(is_not_empty_string(birth_place));
 	char *part = (char*) malloc((FIFTEEN) * sizeof(char*));
-	char *result = (char*) malloc((five) * sizeof(char*));
+	char *result = (char*) malloc((FIVE) * sizeof(char*));
     rewind(places_file);
-	while (fgets(part, fourtysix, places_file))
+	while (fgets(part, FOURTYSIX, places_file))
     	result = get_result(birth_place,part,result);
     assert(is_valid_town_code(result));
     return result;
